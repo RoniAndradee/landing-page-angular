2e5fdaed9a95a8dbdcffb2da766633ec7c5e1e22
roniandradee <ronielli.santos@sptech.school>
2024-08-23 15:11:31 -0300

    terminando home e btn

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        HeaderComponent,
+        NgOptimizedImage,
+        BtnPrimaryComponent
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
